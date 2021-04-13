3e5e9b1344dac64e8345e7e395d8ca8986e2a16c
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-04-13 17:09:13 +0200

    adding typeorm module

--- src/find-profs/professional/professional.module.ts
+++ src/find-profs/professional/professional.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProfessionalController } from './professional.controller';
 import { ProfessionalService } from './professional.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Professional } from "../entities/Professional";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Professional])],
   controllers: [ProfessionalController],
-  providers: [ProfessionalService]
+  providers: [ProfessionalService],
+  exports: [TypeOrmModule]
 })
 export class ProfessionalModule {}
