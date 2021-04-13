455ca9194fbb42fae83b8c10ef63f4736b8753cc
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-04-13 17:10:03 +0200

    adding prof end points

--- src/find-profs/professional/professional.controller.ts
+++ src/find-profs/professional/professional.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Query, Req } from "@nestjs/common";
+import { ProfessionalService } from './professional.service';
+import { Professional } from "../entities/Professional";
+import { Request } from "@nestjs/common";
 
 @Controller('professional')
-export class ProfessionalController {}
+export class ProfessionalController {
+
+  constructor(private professionalService: ProfessionalService) {
+  }
+
+  @Get('all')
+  async getAllProfs(): Promise<Professional[]> {
+    return await this.professionalService.findAllProfessionals();
+  }
+
+  @Get('all_with_review')
+  async getAllProfsWithReview(): Promise<Professional[]> {
+    return await this.professionalService.findAllProfsWithReview();
+  }
+
+  @Get('one_by_id/:id')
+  async getProfById(@Param('id') id: number): Promise<Professional> {
+    return await this.professionalService.findProfById(id);
+  }
+
+  @Get('one_by_id_with_review/:id')
+  async getProfByIdWithReview(@Param('id') id: number): Promise<Professional> {
+    return await this.professionalService.findProfByIdWithReview(id);
+  }
+
+
+  @Get('one_by_name_with_review')
+  async getProfByFirstLastNameWithReview(@Body() body): Promise<Professional> {
+    const { firstName, lastName } = body;
+    console.log(body);
+    console.log(`firstName: ${firstName}, lastName: ${lastName}`);
+    return await this.professionalService.findProfByFirstLastName(firstName, lastName);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
