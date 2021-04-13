7d50ec0343fc81ebcd166ecadee44617eeca6dc1
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-04-13 08:49:24 +0200

    centralised module

---
+++ src/find-profs/profs.module.ts
@@ -0,0 +1,52 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config';
+
+import { UserModule } from "./user/user.module";
+import { ProfessionalModule } from "./professional/professional.module";
+import { RendezVousModule } from "./rendez-vous/rendez-vous.module";
+import { ReviewModule } from "./review/review.module";
+
+import { Professional } from "./entities/Professional";
+import { User } from "./entities/User";
+import { RendezVous } from "./entities/RendezVous";
+import { Review } from "./entities/Review";
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWD,
+    database: process.env.DB_DATABASE,
+    entities: [
+      Professional,
+      User,
+      RendezVous,
+      Review
+    ],
+    synchronize: Boolean(process.env.DB_SYNC)
+    }),
+    UserModule,
+    ProfessionalModule,
+    RendezVousModule,
+    ReviewModule
+  ]
+})
+export class ProfsModule {}
+
+
+
+
+
+
+
+
+
+
+
+
