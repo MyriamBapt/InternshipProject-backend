5cf329b74081c461c6d6a86446c06ba4964436f2
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-04-14 14:31:43 +0200

    adding findRendezVousByProfId method

--- src/find-profs/rendez-vous/rendez-vous.service.ts
+++ src/find-profs/rendez-vous/rendez-vous.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { RendezVous } from "../entities/RendezVous";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class RendezVousService {}
+export class RendezVousService {
+
+  constructor(
+    @InjectRepository(RendezVous)
+    private readonly rendRepo: Repository<RendezVous>
+  ) {}
+
+  async findAllRendezVousByProfId(profId: number): Promise<RendezVous[] | undefined> {
+    const allRendezvous = await this.rendRepo
+      .createQueryBuilder('rendezvous')
+      .innerJoinAndSelect("rendezvous.user", "user")
+      .innerJoinAndSelect("rendezvous.professional", "professional")
+      .where("professional.id = :id", { id: profId })
+      .getMany();
+
+    if (!allRendezvous) {
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'The professional was not found baby!!',
+      }, HttpStatus.NOT_FOUND);
+    }
+
+    return allRendezvous;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
