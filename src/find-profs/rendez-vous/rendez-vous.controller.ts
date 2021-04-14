273f4566e9ab2f7f27e774925691314c29077be5
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-04-14 14:32:11 +0200

    adding getAllRendezVousByProfId method

--- src/find-profs/rendez-vous/rendez-vous.controller.ts
+++ src/find-profs/rendez-vous/rendez-vous.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from "@nestjs/common";
+import { RendezVousService } from "./rendez-vous.service";
+import { RendezVous } from "../entities/RendezVous";
 
-@Controller('rendez-vous')
-export class RendezVousController {}
+@Controller('rendezvous')
+export class RendezVousController {
+
+  constructor(private rendezvousService: RendezVousService ) {
+  }
+
+  @Get('all_by_prof_id/:id')
+  async getAllRendezVousByProfId(@Param('id') id: number): Promise<RendezVous[]> {
+    return await this.rendezvousService.findAllRendezVousByProfId(id);
+  }
+
+}
