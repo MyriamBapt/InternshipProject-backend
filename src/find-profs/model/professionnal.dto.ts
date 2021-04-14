fea6ef807f80641961ccecdfe6c5600f2c963c2a
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-04-14 14:34:23 +0200

    adding validation annotations

--- src/find-profs/model/professionnal.dto.ts
+++ src/find-profs/model/professionnal.dto.ts
@@ -1,14 +1,68 @@
+import {
+  IsArray, IsCurrency,
+  IsEmail, IsFQDN,
+  IsInt, IsMobilePhone,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+  MinLength
+} from "class-validator";
+
 export class ProfessionnalDto {
   id?: number;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(50)
   firstName: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(80)
   lastName: string;
-  email: string;
+
+  @IsEmail()
+  @MaxLength(80)
+  email: string;professional
+
+  @IsString()
+  @MinLength(10)
+  @MaxLength(10)
+  @IsMobilePhone()
   phone: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   city: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   occupation: string;
+
+  @IsInt()
+  @IsNotEmpty()
   yearsActivity: number;
+
+  @IsArray()
   specs: string[];
+
+  @Min(5)
+  @Max(200)
+  @IsCurrency()
   firstMeetingPrice: number;
+
+  @Min(5)
+  @Max(200)
+  @IsCurrency()
   followupMeetingPrice: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsFQDN()
   avatarUrl: string;
 }
