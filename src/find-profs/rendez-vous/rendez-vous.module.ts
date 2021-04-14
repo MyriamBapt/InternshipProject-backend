9ecadc283595fc5fff8ce64906460db320774f22
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-04-14 14:32:29 +0200

    adding TypeOrm module

--- src/find-profs/rendez-vous/rendez-vous.module.ts
+++ src/find-profs/rendez-vous/rendez-vous.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { RendezVousController } from './rendez-vous.controller';
 import { RendezVousService } from './rendez-vous.service';
+import { RendezVous } from "../entities/RendezVous";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RendezVous])],
   controllers: [RendezVousController],
-  providers: [RendezVousService]
+  providers: [RendezVousService],
+  exports: [TypeOrmModule]
 })
 export class RendezVousModule {}
