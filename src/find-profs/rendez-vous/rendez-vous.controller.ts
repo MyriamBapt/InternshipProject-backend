0389ba600e38fe3d52d5f798541450e75cdfe1cb
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-04-18 16:56:55 +0200

    first rendezvous endpoint

--- src/find-profs/rendez-vous/rendez-vous.controller.ts
+++ src/find-profs/rendez-vous/rendez-vous.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, HttpStatus, Param, ParseIntPipe } from "@nestjs/common";
 import { RendezVousService } from "./rendez-vous.service";
 import { RendezVous } from "../entities/RendezVous";
 
@@ -9,7 +9,10 @@ export class RendezVousController {
   }
 
   @Get('all_by_prof_id/:id')
-  async getAllRendezVousByProfId(@Param('id') id: number): Promise<RendezVous[]> {
+  async getAllRendezVousByProfId(
+    @Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }))
+      id: number,
+  ): Promise<RendezVous[]> {
     return await this.rendezvousService.findAllRendezVousByProfId(id);
   }
 
