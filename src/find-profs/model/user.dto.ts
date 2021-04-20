ef8ba60cb4b51d826ea30210ac38b57d80fbcef4
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2021-04-20 10:53:49 +0200

    new user dto to represent production database

--- src/find-profs/model/user.dto.ts
+++ src/find-profs/model/user.dto.ts
@@ -1,11 +1,36 @@
+import { Type } from "class-transformer";
+import { IsDate } from "class-validator";
+import Gender from "./gender";
+
 export class UserDto {
   id?: number;
-  firstName: string;
-  lastName: string;
+  name: string;
+  surname: string;
   email: string;
-  age: string;
+  pseudo: string;
+  password: string;
+  @Type(() => Date)
+  @IsDate()
+  created_at: Date;
+  @Type(() => Date)
+  @IsDate()
+  updated_at: Date;
   height: number;
-  weight: number;
-  other_info: string;
-  avatarUrl: string;
+  gender: Gender;
+  one_signal_player_id: string;
+  auth_method: string;
+  photo_id: string;
+  @Type(() => Date)
+  @IsDate()
+  birth_date: Date;
+  profile_completed: boolean;
+  from_firebase: boolean;
+  @Type(() => Date)
+  @IsDate()
+  last_connection: Date;
+  target_weight: number;
+  original_weight: number;
+  target_loose_weight: number;
+  fb_id: string;
+  apple_id: string;
 }
