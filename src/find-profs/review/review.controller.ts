c569c285fa27ef3d792ce24cf0fdaddb714243a2
MyriamBapt <74616145+MyriamBapt@users.noreply.github.com>
2021-04-23 17:30:41 +0200

    adding getreviews, getreviewsbyUserId, getReviewsbyproId. need little adjustments

--- src/find-profs/review/review.controller.ts
+++ src/find-profs/review/review.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, ParseIntPipe } from '@nestjs/common';
+import { ReviewService } from './review.service';
+import { Review } from '../entities/Review';
 
 @Controller('review')
-export class ReviewController {}
+export class ReviewController {
+
+  constructor(private reviewService: ReviewService) {}
+
+  @Get('all')
+  async getAllReviews(): Promise<Review[]>{
+    return this.reviewService.findAllReviews();
+  }
+
+  @Get('all_by_user_id')
+  async getAllByUserId(@Param ('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }))
+    id: number
+  ): Promise<Review[]>{
+    return this.reviewService.findReviewsByUserId(id);
+  }
+
+  @Get('all_by_professional_id')
+  async getAllByProfessionnalId(@Param ('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }))
+    id: number
+  ): Promise<Review[]>{
+    return this.reviewService.findReviewsByProfessionalId(id);
+  }
+
+}
--- src/find-profs/review/review.service.ts
+++ src/find-profs/review/review.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Review } from '../entities/Review';
+import { Repository } from "typeorm";
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+
+  constructor(
+    @InjectRepository(Review)
+    private readonly reviewRepo:  Repository<Review>
+  ) {}
+
+  async findAllReviews(): Promise<Review[] | undefined>{
+    const allReviews = await this.reviewRepo
+      .createQueryBuilder("reviews")
+      .innerJoinAndSelect("reviews.user", "user")
+      .innerJoinAndSelect("reviews.professional", "professional")
+      .getMany();
+
+    if(!allReviews){
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'Could not find reviews',
+      }, HttpStatus.NOT_FOUND);
+    }
+
+    return allReviews;
+  }
+
+  async findReviewsByUserId(id: number): Promise<Review[] | undefined>{
+    const reviewsByUser = await this.reviewRepo
+      .createQueryBuilder("reviews")
+      .innerJoinAndSelect("reviews.user", "user")
+      .innerJoinAndSelect("reviews.professional", "professional")
+      .where("user.id = :id", { id: id })
+      .getMany();
+
+    if(!reviewsByUser){
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'Could not find reviews for this user',
+      }, HttpStatus.NOT_FOUND);
+    }
+    return reviewsByUser;
+  }
+
+  async findReviewsByProfessionalId(id: number): Promise<Review[] | undefined>{
+    const reviewsByPro = await this.reviewRepo
+      .createQueryBuilder("reviews")
+      .innerJoinAndSelect("review.user", "user")
+      .innerJoinAndSelect("review.professional", "professional")
+      .where("professional.id = :id", { id: id })
+      .getMany();
+
+    if(!reviewsByPro){
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'Could not find reviews for this professional',
+      }, HttpStatus.NOT_FOUND);
+    }
+
+    return reviewsByPro;
+  }
+}
