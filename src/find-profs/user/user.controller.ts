dad2a9f52b276b3de672d1daf5ae4d3ad88d380b
MyriamBapt <74616145+MyriamBapt@users.noreply.github.com>
2021-04-23 10:43:23 +0200

    created endpoints to get all users, user by id, all with Userweight, by id with UserWeight

--- src/find-profs/user/user.controller.ts
+++ src/find-profs/user/user.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, ParseIntPipe } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from '../entities/User';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+  constructor(private userService: UserService) {
+  }
+
+  @Get ('all')
+  async getAllUsers(): Promise<User[]>{
+    return await this.userService.findAllUsers();
+  }
+
+  @Get ('all_with_weight')
+  async getAllUsersWithWeight(): Promise<User[]>{
+    return await this.userService.findAllUsersWithWeight();
+  }
+
+  @Get ('user_by_id/:id')
+  async getUserById
+      (@Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }))
+       id: number
+      ): Promise<User>{
+    return await this.userService.findUserById(id);
+  }
+
+  @Get ('user_by_id_weight/:id')
+  async getUserWeightById
+  (@Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }))
+     id: number
+  ): Promise<User>{
+    return await this.userService.findUserWeightById(id);
+  }
+
+
+}
